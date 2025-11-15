f7e14ea02dcc2a1a70e53106a59327f926247b36
João Pedro Sandri Terra <jpstgamersandriterra1@gmail.com>
2025-11-15 16:04:20 -0300

    aula_4

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
 console.log("Isso é um console.log!")
+
+var primeiroNome = "João Pedro ";
+var segundoNome = " Sandri Terra";
+console.log(primeiroNome + segundoNome);
